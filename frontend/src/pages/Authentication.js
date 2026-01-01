16b6a46778e0250fd4ce2463274c7437d976f7e8
Steve Tyler <steve.tyler75@gmail.com>
2026-01-01 21:40:50 +0000

    sectino 391

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,43 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw Response.json({ message: 'Unsupported mode.' }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  console.log('auth routeauthData', authData);
+
+  const response = await fetch('http://localhost:8080/' + (mode === 'login' ? 'login' : 'signup'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
+
+  console.log('response', response);
+  if (response.status === 422 || response.status === 401) {
+    return response; // react-router will handle this response and extract the data
+  }
+
+  if (!response.ok) {
+    throw Response.json(
+      { message: 'Could not authenticate user.' },
+      { status: 500 }
+    );
+  }
+
+  // manage the token, e.g., store in localStorage or context
+  return redirect('/');
+}
